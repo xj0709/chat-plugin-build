a7780c51c1902b71d517b3ffa78a382dcb914ca3
xj0709 <xjin0721@163.com>
2024-06-20 13:05:48 +0800

    update description

--- src/data-dev.ts
+++ src/data-dev.ts
@@ -2,13 +2,13 @@ export const buildResult = {
     fail: [
         {
           description: 'fail to start the build',
-          name: 'build2',
+          name: 'CXS101289_R245C92',
         }
       ],
     ok: [
       {
-        description: 'build is start successfully',
-        name: 'build1',
+        description: 'build is started successfully',
+        name: 'CXS101289_R245C93',
       },
     ],
   };  
